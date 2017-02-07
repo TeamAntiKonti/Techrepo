5c176b84e9ef5fc07441720173d1501dd02487e4
YavorYanchev <yavor_yanchev_bs@abv.bg>
2017-02-08 00:51:32 +0200

    Added UnitOfWork

---
+++ Techrepo.Repositories/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+using Techrepo.Repositories.Contracts;
+
+namespace Techrepo.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext dbContext;
+
+        public UnitOfWork(DbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public void Commit()
+        {
+            this.dbContext.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
