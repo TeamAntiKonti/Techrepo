01f4dba6a3bbefa735186b78df77294ca1d30129
YavorYanchev <yavor_yanchev_bs@abv.bg>
2017-02-07 16:15:50 +0200

    Initial commit(Structure of the app)

---
+++ Techrepo.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Techrepo.Web.Global" Language="C#" %>
---
+++ Techrepo.Web/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Techrepo.Web.Startup))]
+namespace Techrepo.Web
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
