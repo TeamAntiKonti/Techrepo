1ecd45a3d45e4653a2b28c4e5fd4d0b2cd07c2bc
zorie <shindova@abv.bg>
2017-02-11 15:33:07 +0200

    little fix

---
+++ Techrepo.Data/TechrepoUnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.Entity;
+using Techrepo.Data.Contracts;
+
+namespace Techrepo.Data
+{
+    public class TechrepoUnitOfWork : IUnitOfWork, IDisposable
+    {
+        private DbContext context;
+
+        public TechrepoUnitOfWork(DbContext context)
+        {
+            this.context = context;
+        }
+        public void Commit()
+        {
+            this.context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
