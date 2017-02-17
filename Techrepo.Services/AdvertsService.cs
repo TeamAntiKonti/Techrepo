1a4cabd82001eeb6e954637664ed46e3d435e375
YavorYanchev <yavor_yanchev_bs@abv.bg>
2017-02-17 23:57:59 +0200

    Added adverts service

---
+++ Techrepo.Services/AdvertsService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Techrepo.Data;
+using Techrepo.Models;
+
+namespace Techrepo.Services
+{
+    public class AdvertsService : IAdvertsService
+    {
+        private readonly ITechrepoDbContext techrepoContext;
+
+        public AdvertsService(ITechrepoDbContext techrepoContext)
+        {
+            this.techrepoContext = techrepoContext;
+        }
+
+        public IQueryable<Advert> GetAllAdverts()
+        {
+            return this.techrepoContext.Adverts;
+        }
+    }
+}
