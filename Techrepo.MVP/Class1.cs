36e1fd69bf8bd93fcaaa83650d998c02cd3debf3
YavorYanchev <yavor_yanchev_bs@abv.bg>
2017-02-17 22:28:10 +0200

    Added MVP project

---
+++ Techrepo.MVP/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Techrepo.MVP
+{
+    public class Class1
+    {
+    }
+}
